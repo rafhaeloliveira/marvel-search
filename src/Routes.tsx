78e58d4ca36d8f30e2955d8f43bf79cc49b9b1d2
rafhaeloliveira <rafhaf.oliveira@gmail.com>
2021-10-23 14:39:21 -0300

    adiciona react router

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,34 @@
+// Packages
 import { ThemeProvider } from "@mui/material";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Pages
 import Search from "./page/Search";
+import Results from "./page/Results";
+
+// Provider
 import MarvelTheme from "./provider/ThemeProvider";
+
 
 function Routes() {
   return (
-    <ThemeProvider theme={MarvelTheme}>
-      <Search />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={MarvelTheme}>
+        <Switch>
+          <Route path="/" exact>
+            <Search />
+          </Route>
+          <Route path="/results" exact>
+            <Results />
+          </Route>
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
---
+++ src/page/Results.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Results: FC = () => {
+    return (
+        <h1>Resultados da pesquisa</h1>
+    )
+}
+
+export default Results;
